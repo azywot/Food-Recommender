93ce5ee73367cd66cf5769bcfb2cc7e9e8097134
Agata <82370491+azywot@users.noreply.github.com>
2022-05-29 00:10:17 +0200

    delete unnecessary files

--- interactive-dashboard/agata_proby.R
+++
@@ -1,8 +0,0 @@
-data <- read.csv("data/food_coded.csv")
-h <- hist(!is.na(as.numeric(data$GPA)))
-h
-data$GPA
-data_new <- data[!is.na(data$breakfast), ]      # Delete rows
-data_new 
-hist(data_new$breakfast, main = "AHA")
-               
